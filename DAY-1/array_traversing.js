05603518de26770f30362d4cef44a524b93850db
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-09-18 21:52:27 +0530

    DSA day 1

---
+++ DAY-1/array_traversing.js
@@ -0,0 +1,11 @@
+let arr = [10,12,15,0,8,7,70,20,30,40,50,60]
+
+// Traversing of array
+for (let i = 0; i<arr.length; i++){
+    document.write(`Array of ${i}th element is ${arr[i]} <br><br>`)
+}
+
+// Accessing of array
+
+document.write(arr[5])
+
