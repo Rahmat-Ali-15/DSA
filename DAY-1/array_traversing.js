36406767656ec4b73c9f862e7dd2cb2945bf5e34
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-09-19 21:38:21 +0530

    Modified code

--- DAY-1/array_traversing.js
+++ DAY-1/array_traversing.js
@@ -9,3 +9,18 @@ for (let i = 0; i<arr.length; i++){
 
 document.write(arr[5])
 
+// 
+
+let data = [10,12,15,0,8,7,70,20,30,40,50,60]
+        function getElement() {
+            let inputBox = document.getElementById("input");
+            let element = inputBox.value;
+
+            if ( element >= 0 && element<data.length){
+                alert(data[element]);
+            }
+            else{
+                alert("Please enter valid input");
+            }
+            inputBox.value = ""
+        }
