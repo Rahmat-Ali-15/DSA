c204650e7c8c8d7b0f5279b92a6215b45d0e6441
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-03 02:52:22 +0530

    Code for searching element & finding index of an element form an array

--- DAY-4/searchEl.js
+++ DAY-4/searchEl.js
@@ -1,11 +1,47 @@
-let arr = [10,15,20,25,30];
+// searching element form an array
+let arr = [10, 15, 20, 25, 30];
 let element = 40;
 let found = false;
 
-for (let i = 0; i<arr.length; i++){
-    if(arr[i] === element){
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
         found = true;
         break;
     }
 }
-console.log(found);
+// console.log(found);
+
+// getting element index from an array;
+
+let data = [10, 15, 20, 25, 30, 40, 50];
+let el = 25;
+let index = "";
+for (let i = 0; i < data.length; i++) {
+    if (data[i] === el) {
+        index = i;
+    }
+}
+console.log([index]);
+
+
+// getting element index from an array by using input & button
+
+const searchIndex = () => {
+    let data = [10, 15, 20, 25, 30, 40, 50];
+    let el = Number(document.getElementById("input_el").value);
+    let index = "";
+
+    // preventing from taking negative value
+    if(el<0){
+        alert("Element is not present");
+        return
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] === el) {
+            index = i;
+            break
+        }
+    }
+    console.log([index]);
+}
