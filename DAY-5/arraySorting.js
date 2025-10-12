c8886e48c12ea36ffe7a09ce31dd72500d073a5e
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-12 10:08:32 +0530

    Array sorting

---
+++ DAY-5/arraySorting.js
@@ -0,0 +1,13 @@
+let data = [20,5,10,45,2,0,8,30];
+
+for (let i = 0; i<data.length; i++){
+    for(let j = 0; j<data.length; j++){
+        console.log(data)
+        if(data[j]>data[j+1]){
+            let temp = data[j];
+            data[j]=data[j+1];
+            data[j+1] = temp;
+        }
+    }
+}
+console.log(data);
