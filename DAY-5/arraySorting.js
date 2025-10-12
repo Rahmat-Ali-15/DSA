4e8e744a7834393bc1eb26d5cded81c71f2bb43a
Rahmat-Ali-15 <developer.rahmat15@gmail.com>
2025-10-12 10:09:48 +0530

    Code of array sorting

--- DAY-5/arraySorting.js
+++ DAY-5/arraySorting.js
@@ -2,7 +2,7 @@ let data = [20,5,10,45,2,0,8,30];
 
 for (let i = 0; i<data.length; i++){
     for(let j = 0; j<data.length; j++){
-        console.log(data)
+        // console.log(data)
         if(data[j]>data[j+1]){
             let temp = data[j];
             data[j]=data[j+1];
